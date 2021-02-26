1f97bf6c3040d7c3e4e6104d5887266a486ac1c8
marc655 <marc.biester@gmail.com>
2021-02-26 19:22:58 +0100

    made some changes for more randomness

--- streamlines/main.py
+++ streamlines/main.py
@@ -1,6 +1,7 @@
 from streamlines import streamLines
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 grid = {
     "x_start": -5,
@@ -10,23 +11,46 @@ grid = {
     "no_points_x": 100,
     "no_points_y": 100,
 }
+pic = 5
+while True:
+    try:
+        t1 = datetime.datetime.now()
+        self = streamLines(grid)
 
-self = streamLines(grid)
+        for i in range(np.random.randint(1, 200)):
+            x, y = np.random.uniform(-5, 5), np.random.uniform(-3.5, 3.5)
+            self.add_vortex(np.random.uniform(-10, 10), x, y)
+            self.add_source_sink(np.random.uniform(-3, 3), x, y)
 
-for i in range(50):
-    x, y = np.random.uniform(-5, 5), np.random.uniform(-3.5, 3.5)
-    self.add_vortex(np.random.choice([-1,1])*4, x, y) 
-    self.add_source_sink(-0.1, x, y)
-    
-# for i in range(200):
-#     x, y = np.random.uniform(-5, 5), np.random.uniform(-3.5, 3.5)
-#     self.add_source_sink(2, x, y)
+        for i in range(np.random.randint(1, 200)):
+            x, y = np.random.uniform(-5, 5), np.random.uniform(-3.5, 3.5)
+            self.add_source_sink(np.random.uniform(-3, 3), x, y)
 
+        plt.figure(figsize=(420 / 25.4, 297 / 25.4))
+        random_seed_setting = np.random.choice(['random','sources','grid'], np.random.randint(1,4)).tolist()
+        self.calc_streamtraces(
+            n_streamtraces=np.random.randint(100, 1000),
+            seeds=random_seed_setting,
+            dt=0.001,
+            maxiter=np.random.randint(200, 8000),
+            radius=np.random.uniform(0.02, 0.2),
+            n_cpu=4,
+        )
+        t2 = datetime.datetime.now()
+        print(
+            f"it took {t2-t1} to calculate the flow field and the stream traces for {pic}"
+        )
 
-plt.figure(figsize=(420 / 25.4, 297 / 25.4))
-self.calc_streamtraces(n_streamtraces=1000, seeds=['grid', 'sources'], dt=0.001, maxiter=2000, radius=0.1)
-for ps in self.streamtraces:
-    plt.plot([x[0] for x in ps], [x[1] for x in ps], color='black', alpha=0.5)
-plt.axis('equal')
-plt.axis("off")
-plt.savefig('bla.svg')
+        for ps in self.streamtraces:
+            plt.plot([x[0] for x in ps], [x[1] for x in ps], color="black", alpha=0.5)
+        plt.axis("equal")
+        plt.axis("off")
+        plt.tight_layout()
+        plt.savefig(f"{pic}.svg")
+        plt.savefig(f"{pic}.png")
+
+        t3 = datetime.datetime.now()
+        print(f"it took {t3-t2} to create plot {pic}")
+    except Exception as ex:
+        print(ex)
+    pic += 1
